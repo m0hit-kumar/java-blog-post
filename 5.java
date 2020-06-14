8175d46a7cdf430d872fb3405c423adaebf44d8d
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2020-06-14 22:42:41 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		int a,b,c,d,x=14,y=7;
+		a= x+y ;
+		b=x-y;
+		c=x*y;
+		d=x/y;
+		System.out.print(a +"\n");
+		System.out.print(b + "\n");
+		System.out.print(c +"\n");
+		System.out.print(d);
+		
+	}
+}
