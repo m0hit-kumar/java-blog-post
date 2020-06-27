1c63d07d47dc3914d060dbf8bcbdd90697051e30
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2020-06-27 19:45:41 +0530

    Update 19.java

--- 19.java
+++ 19.java
@@ -1,1 +1,25 @@
 
+public class Main {    
+    public static void main(String[] args) {        
+                
+        int A[]= new int [] {10, 20, 30, 40, 50};     
+         
+        int B[] = new int[A.length];    
+       
+        for (int i = 0; i < A.length; i++) {     
+            B[i] = A[i];     
+        }      
+
+        System.out.println("Elements of array A: ");    
+        for (int i = 0; i < A.length; i++) {     
+           System.out.print(A[i] + " ");    
+        }     
+            
+        System.out.println();    
+      
+        System.out.println("Elements of array B are following");    
+        for (int i = 0; i < B.length; i++) {     
+           System.out.print(B[i] + " ");    
+        }     
+    }    
+}    
